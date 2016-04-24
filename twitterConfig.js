b26045c1c9523923cf40188590fa5d1f21cdb22c
Michaeltym <1412909864@qq.com>
2016-04-24 21:52:09 +1000

    added twitter key

--- twitterConfig.js
+++ twitterConfig.js
@@ -3,7 +3,7 @@ var twitterKeys = require('./constant/twitterApps');
 
 clients = [];
 
-for (var i = 0; i < 8; i++) {
+for (var i = 0; i < 11; i++) {
     var client = new Twitter({
         consumer_key: twitterKeys[i].consumer_key,
         consumer_secret: twitterKeys[i].consumer_secret,
