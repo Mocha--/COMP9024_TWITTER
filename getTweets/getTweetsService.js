8dba8214011915790d799e3c9d220e26f9cfe7b6
Michaeltym <1412909864@qq.com>
2016-04-26 13:27:33 +1000

    added get keywords

--- getTweets/getTweetsService.js
+++ getTweets/getTweetsService.js
@@ -25,24 +25,44 @@ function getLocations(jsonData) {
 }
 
 function getTweets(jsonData, clientNumber) {
+    var locations = getLocations(jsonData);
     Twitter.clients[clientNumber].stream('statuses/filter', {
-        track: getLocations(jsonData)
+        track: locations
     }, function(stream) {
         stream.on('data', function(tweet) {
             if (tweet.user) {
-                userName = tweet.user.name || "****";
-                screenName = tweet.user.screen_name || "****";
-                created = tweet.created_at || "****";
-                location = tweet.user.location || "****";
-                retweetCount = tweet.retweet_count;
-                text = tweet.text || "****";
-                console.log([userName, screenName, created, location, retweetCount, text]);
+                var keywords = getKeywords(jsonData, tweet.text);
+                var userName = tweet.user.name || "****";
+                var screenName = tweet.user.screen_name || "****";
+                var created = tweet.created_at || "****";
+                var location = tweet.user.location || "****";
+                var retweetCount = tweet.retweet_count;
+                var text = tweet.text || "****";
+                console.log(keywords)
+                console.log("******************************")
+                // console.log([userName, screenName, created, location, retweetCount, text]);
             }
         });
         stream.on('error', function(error) {
             throw error;
         });
     });
+}
+
+function getKeywords(jsonData, text) {
+    var locations = getLocations(jsonData).toLowerCase();
+    var locationsArray = locations.split(",");
+    var wordsArray = text.split(" ");
+    var keywords = [];
+    for(var i = 0; i < wordsArray.length; i++) {
+        if(locationsArray.indexOf(wordsArray[i].toLowerCase()) >= 0) {
+            if(keywords.indexOf(wordsArray[i].toLowerCase()) == -1) {
+                keywords.push(wordsArray[i].toLowerCase());
+            }
+            
+        }
+    }
+    return keywords;
 }
 
 exports.getTweets = getTweets;
