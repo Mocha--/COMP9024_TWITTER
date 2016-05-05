18c60b5e4dcf2646a383515b877ab9b1ef21a95c
Mocha <xibowangmelb@gmail.com>
2016-05-06 00:57:53 +1000

    process raw data

--- test.js
+++ test.js
@@ -1,24 +1,32 @@
 'use strict';
 
 let nano = require('nano')('http://115.146.85.141:5984/');
-let testDB = nano.use('mapreducetest');
+let testDB = nano.use('twittes');
 
-// testDB.insert({
-//     lanuage: 'javascript',
-//     views: {
-//         testView1: {
-//             map: function(doc) {
-//                 emit(1, 2);
-//             },
-//             reduce: function(key, value) {
-//                 return sum(value)
-//             }
-//         }
-//     }
-// }, '_design/test1', function(err, res) {
-//     console.log(err);
+testDB.insert({
+    lanuage: 'javascript',
+    views: {
+        filterEmptyLocation: {
+            map: function(doc) {
+                if(doc.location !== '****'){
+                    emit(, 2);
+                }
+            },
+            reduce: function(key, value) {
+                return sum(value)
+            }
+        }
+    }
+}, '_design/test1', function(err, res) {
+    console.log(err);
+})
+
+// testDB.view('test1', 'testView1', function(err, res){
+//     console.log(res);
 // })
 
-testDB.view('test1', 'testView1', function(err, res){
-    console.log(res);
-})
+// testDB.list({
+//     location: 'melbourne'
+// }, function(err, res) {
+//     console.log(res);
+// })
