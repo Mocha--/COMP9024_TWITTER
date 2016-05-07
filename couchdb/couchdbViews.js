7fcde8e84fc7ab26249d4752ada170aa725e38e6
Mocha <xibowangmelb@gmail.com>
2016-05-08 02:24:34 +1000

    couchdb view finished

--- couchdb/couchdbViews.js
+++ couchdb/couchdbViews.js
@@ -22,6 +22,58 @@ let sample = {
     "overseas": false
 };
 
+// baseOnTo
+// db.insert({
+//     'language': 'javascript',
+//     views: {
+//         overseasContinentWithAttitude: {
+//             map: function(doc) {
+//                 if (doc.overseas && doc.to.continent && doc.attitude) {
+//                     emit([doc.to.continent, doc.attitude], 1);
+//                 }
+//             },
+//             reduce: function(keys, values) {
+//                 return sum(values);
+//             }
+//         },
+//         countryInContinent: {
+//             map: function(doc) {
+//                 if (doc.overseas && doc.to.continent && doc.to.country) {
+//                     emit([doc.to.continent, doc.to.country], 1);
+//                 }
+//             },
+//             reduce: function(keys, values) {
+//                 return sum(values);
+//             }
+//         },
+//         domesticStateWithAttitude: {
+//             map: function(doc) {
+//                 if (!doc.overseas && doc.to.state && doc.attitude) {
+//                     emit([doc.to.state, doc.attitude], 1);
+//                 }
+//             },
+//             reduce: function(keys, values) {
+//                 return sum(values);
+//             }
+//         },
+//         cityInState: {
+//             map: function(doc) {
+//                 if (!doc.overseas && doc.to.state && doc.to.city) {
+//                     emit([doc.to.state, doc.to.city], 1);
+//                 }
+//             },
+//             reduce: function(keys, values) {
+//                 return sum(values);
+//             }
+//         }
+//     }
+// }, '_design/baseOnTo', function(err, res) {
+//     if (!err) {
+//         console.log(res);
+//     }
+// })
+
+// overall
 // db.insert({
 //     language: 'javascript',
 //     views: {
@@ -54,22 +106,43 @@ let sample = {
 //     }
 // })
 
-db.insert({
-    language: 'javascript',
-    views: {
-        overseasVsDomestic: {
-            map: function(doc) {
-                if (doc.from.city) {
-                    emit([doc.from.city, doc.overseas], 1);
-                }
-            },
-            reduce: function(keys, values) {
-                return sum(values);
-            }
-        }
-    }
-}, '_design/baseOnFrom', function(err, res) {
-    if (!err) {
-        console.log(res);
-    }
-});
+// basedOnFrom
+// db.insert({
+//     language: 'javascript',
+//     views: {
+//         overseasVsDomestic: {
+//             map: function(doc) {
+//                 if (doc.from.city) {
+//                     emit([doc.from.city, doc.overseas], 1);
+//                 }
+//             },
+//             reduce: function(keys, values) {
+//                 return sum(values);
+//             }
+//         },
+//         melVsSyd: {
+//             map: function(doc) {
+//                 if (!doc.overseas && doc.to.state && (doc.from.city === 'melbourne' || doc.from.city === 'sydney')) {
+//                     emit([doc.from.city, doc.to.state], 1);
+//                 }
+//             },
+//             reduce: function(keys, values) {
+//                 return sum(values);
+//             }
+//         },
+//         states: {
+//             map: function(doc) {
+//                 if (doc.from.state) {
+//                     emit(doc.from.state, 1);
+//                 }
+//             },
+//             reduce: function(keys, values) {
+//                 return sum(values);
+//             }
+//         }
+//     }
+// }, '_design/baseOnFrom', function(err, res) {
+//     if (!err) {
+//         console.log(res);
+//     }
+// });
