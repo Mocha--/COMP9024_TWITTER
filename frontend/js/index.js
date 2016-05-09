5d4e66aaf5c5dc9f9b6380a521955e4465fec35d
Yuqing Han <yuqingh@student.unimelb.edu.au>
2016-05-09 20:22:28 +1000

    some updates

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -201,6 +201,72 @@ function loadchart(index) {
                 data: negative
             }]
         });
+    }//
+
+    if(index === 2) {
+        $.getJSON('http://localhost:8080/overall/countryCount', function (data) {
+
+        // Add lower case codes to the data set for inclusion in the tooltip.pointFormat
+        $.each(data, function () {
+            console.log(this);
+            this.flag = this.code.replace('UK', 'GB').toLowerCase();
+        });
+
+        // Initiate the chart
+        $('#heatmap').highcharts('Map', {
+
+            title: {
+                text: 'Tourists Distribution'
+            },
+
+            legend: {
+                title: {
+                    text: 'Number of Tweets',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.textColor) || 'black'
+                    }
+                }
+            },
+
+            mapNavigation: {
+                enabled: true,
+                buttonOptions: {
+                    verticalAlign: 'bottom'
+                }
+            },
+
+            tooltip: {
+                backgroundColor: 'none',
+                borderWidth: 0,
+                shadow: false,
+                useHTML: true,
+                padding: 0,
+                pointFormat: '<span class="f32"><span class="flag {point.flag}"></span></span>' +
+                    ' {point.name}: <b>{point.value}</b>',
+                positioner: function () {
+                    return { x: 0, y: 250 };
+                }
+            },
+
+            colorAxis: {
+                min: 1,
+                max: 1000,
+                type: 'logarithmic'
+            },
+
+            series : [{
+                data : data,
+                mapData: Highcharts.maps['custom/world'],
+                joinBy: ['iso-a2', 'code'],
+                name: 'Number of Tweets',
+                states: {
+                    hover: {
+                        color: '#BADA55'
+                    }
+                }
+            }]
+        });
+    });
     }
 
     if(index === 3) {
