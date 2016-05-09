2f1ccc661a0a35047d490e3b86c77f1a76e10386
Michaeltym <1412909864@qq.com>
2016-05-09 20:15:04 +1000

    added convert data functions

---
+++ backend/aurin.js
@@ -0,0 +1,48 @@
+var income = [{
+		state: "capital territory",
+		wage: 88270,
+	}, {
+		state: "western australia",
+		wage: 87001,
+	}, {
+		state: "new south wales",
+		wage: 77600,
+	}, {
+		state: "queensland",
+		wage: 75759,
+	}, {
+		state: "north territory",
+		wage: 75603
+	}, {
+		state: "victoria",
+		wage: 72623
+	}, {
+		state: "tasmania",
+		wage: 65759
+	}];
+
+var population = [{
+		state: "new south wales",
+		population: 7644200,
+	}, {
+		state: "victoria",
+		population: 5966700, 
+	}, {
+		state: "queensland",
+		population: 4792900,
+	}, {
+		state: "western australia",
+		population: 2598200,
+	}, {
+		state: "tasmania",
+		population: 517200,
+	}, {
+		state: "capital territory",
+		population: 392000
+	}, {
+		state: "northern territory",
+		population: 244500,
+	}];
+
+exports.income = income;
+exports.population = population;
